d28f72fe85ac0e65b18324aa7c74d3949a4156d3
Filip Domin <filip.domin@esky.com>
2020-04-28 18:37:33 +0200

    Added Prototype pattern example

---
+++ DesignPatterns/PrototypePattern/Manager/PrototypeManager.cs
@@ -0,0 +1,16 @@
+﻿using PrototypePattern.Prototype;
+using System.Collections.Generic;
+
+namespace PrototypePattern.Manager
+{
+    public class PrototypeManager
+    {
+        private Dictionary<string, IPrototype> _prototypes = new Dictionary<string, IPrototype>();
+
+        public IPrototype this[string key]
+        {
+            get => _prototypes[key];
+            set => _prototypes.Add(key, value);
+        }
+    }
+}
---
+++ DesignPatterns/PrototypePattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using PrototypePattern.Manager;
+using PrototypePattern.Prototype;
+using System.Collections.Generic;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prototypeManager = new PrototypeManager();
+
+            var order1 = new OrderDetails()
+            {
+                OrderId = 101,
+                OrderItems = new List<string>() { "Pizza", "Cake", "Soda" },
+                TotalValue = 23.50m
+            };            
+            
+            var order2 = new OrderDetails()
+            {
+                OrderId = 102,
+                OrderItems = new List<string>() { "Cheeseburger", "Hamburger", "Soda", "Chicken strips" },
+                TotalValue = 32.80m
+            };
+
+            prototypeManager[$"Order::{order1.OrderId}"] = order1;
+            prototypeManager[$"Order::{order2.OrderId}"] = order2;
+
+            var copiedOrder1 = prototypeManager["Order::101"].Clone() as OrderDetails;
+            var copiedOrder2 = prototypeManager["Order::102"].Clone() as OrderDetails;
+
+            copiedOrder1.Print();
+            copiedOrder2.Print();
+        }
+    }
+}
